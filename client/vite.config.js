98c469d92f28e6160c9585c4ef0b6d5b27c6b5a0
Yash Shinde <yash73shinde@gmail.com>
2025-02-13 17:47:10 +0530

    Change vite config  and backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
-    https: mode === 'development'
-      ? {
-          key: fs.readFileSync(path.resolve(__dirname, "certs/server.key")),
-          cert: fs.readFileSync(path.resolve(__dirname, "certs/server.cert")),
-        }
-      : false, 
-  },
+  //   https: mode === 'development'
+  //     ? {
+  //         key: fs.readFileSync(path.resolve(__dirname, "certs/server.key")),
+  //         cert: fs.readFileSync(path.resolve(__dirname, "certs/server.cert")),
+  //       }
+  //     : false, 
+   },
 }));
